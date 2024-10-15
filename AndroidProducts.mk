68c3006adc4ea41b648cc1d53560e6c4714e8c77
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2024-10-15 03:08:17 +0300

    apollo: PixelOS let's go

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2024 The PixelOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/aosp_apollo.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_apollo-user \
+    aosp_apollo-userdebug \
+    aosp_apollo-eng
