e425367288eca78df3196c4f8431a4ab104636f9
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2025-02-15 14:54:04 +0000

    Revert "apollon: Rename prebuilt audio HAL and libs"
    This reverts commit aa668d6b314efc4a5d676454a8c0f4b7f68cb48e.

--- extract-files.sh
+++ extract-files.sh
@@ -26,10 +26,6 @@ EOF
             sed -i "s/0x10098/0/g" "${2}"
             sed -i "s/0x1F/0x0/g" "${2}"
             ;;
-        vendor/lib/hw/audio.primary.apollo.so)
-            [ "$2" = "" ] && return 0
-            sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_apollo\.so\x00\x00\x00\x00|g" "${2}"
-            ;;
         vendor/lib64/camera/components/com.mi.node.watermark.so)
             [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
